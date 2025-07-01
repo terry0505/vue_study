911e099da10fb1b33511bfa6f6b1561f49b78a02
hana <hana1155@hanmail.net>
2025-07-01 13:12:24 +0900

    1-1 환경세팅

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('안녕하세요, Vue 3!')
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p>Vue 3 튜토리얼에 오신 것을 환영합니다!</p>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    color: #42b983;
+  }
+</style>
