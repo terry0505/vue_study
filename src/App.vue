3db0abd686ad781fb3d5a8f40833bc0d679965f6
hana <hana1155@hanmail.net>
2025-07-01 13:34:12 +0900

    1-2 ref와 reactive

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
-<script setup>
-import {ref} from 'vue'
-
-const message = ref('안녕하세요, Vue 3!')
-</script>
-
 <template>
   <div>
-    <h1>{{ message }}</h1>
-    <p>Vue 3 튜토리얼에 오신 것을 환영합니다!</p>
+    <h2>이름을 입력해보세요.</h2>
+    <input v-model="name" placeholder="이름 입력" />
+    <p>안녕하세요, {{ name }}님!</p>
+
+    <h2>reactive 객체 실습</h2>
+    <input v-model="user.firstName" placeholder="이름 입력" />
+    <input v-model="user.lastName" placeholder="성 입력" />
+    <p>안녕하세요, {{ user.firstName }} {{ user.lastName }}님!</p>
   </div>
 </template>
 
+<script setup>
+import { reactive, ref } from "vue";
+
+const name = ref("홍길동"); //단일 값
+const user = reactive({
+  firstName: "홍",
+  lastName: "길동"
+}); //객체
+</script>
+
 <style scoped>
-  h1 {
-    color: #42b983;
-  }
+input {
+  margin: 4px;
+  padding: 4px;
+}
 </style>
